79c8b705fd8bdb822da3f3af34ca942d4499d720
Safet Salkanović <salkanovicsafet@gmail.com>
2022-02-06 15:00:07 +0100

    Write logic behind Start button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import Intro from "./components/Intro";
 
 export default function App() {
+  const [isIntro, setIsintro] = useState(true);
+
+  function startQuiz() {
+    setIsintro(false);
+  }
+
   return (
-    <div className="main blobs blobs--big">
-      <Intro />
+    <div className={`main blobs ${isIntro ? "blobs--big" : "blobs--small"}`}>
+      {isIntro && <Intro startQuiz={startQuiz} />}
     </div>
   );
 }
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,11 +1,13 @@
-export default function Intro() {
+export default function Intro(props) {
   return (
     <div className="intro">
       <h1 className="intro__title">Weeb trivia</h1>
       <h2 className="intro__description">
-        How big of a weeb are you? Click "Start quiz" and find out!
+        How big of a weeb are you? Click the button below and find out!
       </h2>
-      <button className="intro__btn">Start quiz</button>
+      <button onClick={props.startQuiz} className="intro__btn">
+        Start quiz
+      </button>
     </div>
   );
 }
