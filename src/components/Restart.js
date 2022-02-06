2219154de2c743cf2cfac877084fa4e64b1869c0
Safet Salkanović <salkanovicsafet@gmail.com>
2022-02-06 21:49:30 +0100

    Set up confetti in case of max score result

--- src/components/Restart.js
+++ src/components/Restart.js
@@ -1,7 +1,10 @@
+import Confetti from "react-confetti";
+
 export default function Restart(props) {
   return (
     <div className="restart">
       You scored {props.points}/5 correct answers
+      {props.points === 5 && <Confetti />}
       <button onClick={props.restartGame} className="action__btn btn--restart">
         Play again
       </button>
